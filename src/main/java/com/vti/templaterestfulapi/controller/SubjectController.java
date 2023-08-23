fce5d6c4aea037bacac3e1459c0833e810e8f800
GivngLe <lehuugivng@gmail.com>
2023-08-23 22:57:02 +0700

    Giang
    up

---
+++ src/main/java/com/vti/templaterestfulapi/controller/SubjectController.java
@@ -0,0 +1,31 @@
+package com.vti.templaterestfulapi.controller;
+
+import com.vti.templaterestfulapi.dto.LoginForm;
+import com.vti.templaterestfulapi.models.ResponseObject;
+import com.vti.templaterestfulapi.models2.Subject;
+import com.vti.templaterestfulapi.service.SubjectService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/subject")
+public class SubjectController {
+    @Autowired
+    SubjectService subjectService;
+
+    @PostMapping("/insert")
+    public ResponseEntity<?> insert(@Valid @RequestBody Subject subject) {
+
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new ResponseObject(200, "Ok",
+                        subjectService.create(subject)));
+
+
+    }
+}
---
+++ src/main/java/com/vti/templaterestfulapi/repositories/ProfileRepository.java
@@ -0,0 +1,5 @@
+package com.vti.templaterestfulapi.repositories;
+
+public class ProfileRepository {
+
+}
